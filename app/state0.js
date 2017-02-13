cf515df2472726fbfe77731d522e2d49e5a4c7b4
zila661 <vedran.zilic661@gmail.com>
2017-02-13 19:38:10 +0100

    css

--- app/state0.js
+++ app/state0.js
@@ -4,8 +4,9 @@ demo.state0.prototype = {
     preload: function () {},
     create: function () {
         game.stage.backgroundColor = "#DDDDDD";
-          console.log("state0");
-          addChangeEventListener();
+        console.log("state0");
+        addChangeEventListener();
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
     },
     update: function () {}
@@ -21,15 +22,15 @@ function addKeyCallback(key, fn, args) {
 }
 
 function addChangeEventListener() {
-    
-        addKeyCallback(Phaser.Keyboard.ZERO, changeState, 0);
-        addKeyCallback(Phaser.Keyboard.ONE, changeState, 1);
-        addKeyCallback(Phaser.Keyboard.TWO, changeState, 2);
-        addKeyCallback(Phaser.Keyboard.THREE, changeState, 3);
-        addKeyCallback(Phaser.Keyboard.FOUR, changeState, 4);
-        addKeyCallback(Phaser.Keyboard.FIVE, changeState, 5);
-        addKeyCallback(Phaser.Keyboard.SIX, changeState, 6);
-        addKeyCallback(Phaser.Keyboard.SEVEN, changeState, 7);
-        addKeyCallback(Phaser.Keyboard.EIGHT, changeState, 8);
-        addKeyCallback(Phaser.Keyboard.NINE, changeState, 9);
+
+    addKeyCallback(Phaser.Keyboard.ZERO, changeState, 0);
+    addKeyCallback(Phaser.Keyboard.ONE, changeState, 1);
+    addKeyCallback(Phaser.Keyboard.TWO, changeState, 2);
+    addKeyCallback(Phaser.Keyboard.THREE, changeState, 3);
+    addKeyCallback(Phaser.Keyboard.FOUR, changeState, 4);
+    addKeyCallback(Phaser.Keyboard.FIVE, changeState, 5);
+    addKeyCallback(Phaser.Keyboard.SIX, changeState, 6);
+    addKeyCallback(Phaser.Keyboard.SEVEN, changeState, 7);
+    addKeyCallback(Phaser.Keyboard.EIGHT, changeState, 8);
+    addKeyCallback(Phaser.Keyboard.NINE, changeState, 9);
 }
